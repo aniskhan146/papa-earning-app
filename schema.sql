a3d65f39aa573a82ad5dbf13e66c75e0c80ad36f
ANISUR RAHMAN <114776539+aniskhan146@users.noreply.github.com>
2025-08-07 09:29:35 -0700

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    balance REAL NOT NULL,
-    lifetime_points REAL NOT NULL,
-    history TEXT NOT NULL,
-    last_ad_time REAL NOT NULL,
-    last_daily_bonus TEXT
-);
